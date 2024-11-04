afa6073e99bb44122733dd6a6738f816872bbfed
Will Green <will.green.mha.pmp@gmail.com>
2024-11-04 17:32:38 -0600

    second exercise code added

--- module1lesson2.py
+++ module1lesson2.py
@@ -8,3 +8,16 @@ age = input("How old are you?")
 print(f"I can't believe you are {age} years old!")
 
 print("Please meet " + name + "! Their favorite color is " + favorite_color + ", and they are " + age + " years old!")
+
+
+
+
+exam_score = int(input("Enter your exam score: "))
+
+if exam_score > 90:
+    print("Your exam score is excellent!")
+elif exam_score > 69 and exam_score < 90:
+    print("Your exam score is good!")
+else:
+    print("Your exam score needs improvement!")
+    
