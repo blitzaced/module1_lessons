c75945e97522beb51e175dd751e7a96a35752ca5
Will Green <will.green.mha.pmp@gmail.com>
2024-11-04 17:12:47 -0600

    lesson code added

---
+++ module1lesson2.py
@@ -0,0 +1,10 @@
+name = input("What is your name?")
+print(f"Hello, {name}!")
+
+favorite_color = input("What is your favorite color?")
+print(f"Your favorite color is {favorite_color}!")
+
+age = input("How old are you?")
+print(f"I can't believe you are {age} years old!")
+
+print("Please meet " + name + "! Their favorite color is " + favorite_color + ", and they are " + age + " years old!")
